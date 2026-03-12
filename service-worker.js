ed26f6260c9ae00546d2ac2ad53463cabec71bc7
wowleo74 <wowleo74@gmail.com>
2026-03-12 16:12:09 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const cacheName="english-game-v1";
+
+const filesToCache=[
+"/",
+"/index.html"
+];
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(cacheName)
+.then(cache=>cache.addAll(filesToCache))
+);
+});
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request)
+.then(response=>response || fetch(e.request))
+);
+});
